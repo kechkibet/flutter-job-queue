077e59be15e615d1366a9ec94f1fd2bd7e958017
kech kibet <kech@ol-digital.com>
2023-01-26 12:07:02 +0300

    add: updating current workers synchronized.

--- lib/dart_queue.dart
+++ lib/dart_queue.dart
@@ -1,18 +1,15 @@
 import 'package:dart_queue/SynchronizedQueue.dart';
+import 'package:synchronized/synchronized.dart';
 import 'package:uuid/uuid.dart';
 
-
-
-enum ProcessStatus {
-  Success,
-  Failed
-}
+enum ProcessStatus { Success, Failed }
 
 class LangoQueue {
-  LangoQueue(this.max_workers, this.workerFunction){
+  LangoQueue(this.max_workers, this.workerFunction) {
     // _bootWorkers();
   }
 
+  var lock = Lock();
   final int max_workers;
   int current_workers = 0;
 
@@ -29,39 +26,42 @@ class LangoQueue {
     message["id"] = uuid.v4();
 
     await _queue.add(message);
-    if(current_workers < max_workers){
-      current_workers +=1;
-      _workerLoop();
-
-    }
-  }
-  void _workerLoop()async{
-      final message = await _queue.getUnprocessedMessage();
-
-      if(message == null){
-        print("No more messages to handle, exiting...");
-        return;
-      }
-      var processStatus = ProcessStatus.Failed;
-      try{
-        processStatus = await workerFunction(message);
-      // ignore: empty_catches
-      }catch(e){}
-      switch(processStatus){
-        case ProcessStatus.Success:
-          // TODO: update
-          await _queue.setMessageStatus(message["id"], MessageStatus.Processed);
-          break;
-        case ProcessStatus.Failed:
-          // TODO: Handle this case.
-          await _queue.setMessageStatus(message["id"], MessageStatus.NotProcessed);
-          break;
-      }
-      current_workers -=1;
-      if(current_workers < max_workers){
+    await lock.synchronized(() {
+      if (current_workers < max_workers) {
+        current_workers += 1;
         _workerLoop();
       }
+    });
   }
 
+  void _workerLoop() async {
+    final message = await _queue.getUnprocessedMessage();
 
+    if (message == null) {
+      print("No more messages to handle, exiting...");
+      return;
+    }
+    var processStatus = ProcessStatus.Failed;
+    try {
+      processStatus = await workerFunction(message);
+      // ignore: empty_catches
+    } catch (e) {}
+    switch (processStatus) {
+      case ProcessStatus.Success:
+        // TODO: update
+        await _queue.setMessageStatus(message["id"], MessageStatus.Processed);
+        break;
+      case ProcessStatus.Failed:
+        // TODO: Handle this case.
+        await _queue.setMessageStatus(
+            message["id"], MessageStatus.NotProcessed);
+        break;
+    }
+    await lock.synchronized(() {
+      current_workers -= 1;
+      if (current_workers < max_workers) {
+        _workerLoop();
+      }
+    });
+  }
 }
